589d00350d389bbc477cfdadb7082a313c75c68e
yfxhdxy <60698647+yfxhdxy@users.noreply.github.com>
2022-04-08 17:19:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# blog-client
+ 
---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>Feel free to speak your mind</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
